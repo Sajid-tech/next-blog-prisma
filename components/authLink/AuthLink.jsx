dedd3af9b5eb50cfee887066a72514693e22c2da
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2024-02-28 14:33:51 +0530

    authlink ui

--- components/authLink/AuthLink.jsx
+++ components/authLink/AuthLink.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import styles from "./authLink.module.css";
 
-const AuthLink = () => {
-  return <div>AuthLink</div>;
+const AuthLinks = () => {
+  const status = "unauthenticated";
+
+  return (
+    <>
+      {status === "unauthenticated" ? (
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
+      ) : (
+        <>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+      <div className={styles.burger}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+    </>
+  );
 };
 
-export default AuthLink;
+export default AuthLinks;
