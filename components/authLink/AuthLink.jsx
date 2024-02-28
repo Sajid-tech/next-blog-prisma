4c2b337699cba188ddeaf457f90280bd724bbecb
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2024-02-28 10:18:15 +0530

    intial

---
+++ components/authLink/AuthLink.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthLink = () => {
+  return <div>AuthLink</div>;
+};
+
+export default AuthLink;
---
+++ components/menuCategories/MenuCategories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuCategories = () => {
+  return <div>MenuCategories</div>;
+};
+
+export default MenuCategories;
