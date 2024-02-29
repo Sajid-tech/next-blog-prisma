13f99bf364c8e22480e7251ee048969e76509f66
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2024-02-29 11:41:35 +0530

    pagination ui

--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -10,13 +10,13 @@ const Menu = () => {
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <MenuPosts withImage={false} />
+      <MenuPosts withImage={true} />
       <h2 className={styles.subtitle}>Discover by topic</h2>
       <h1 className={styles.title}>Categories</h1>
       <MenuCategories />
-      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      {/* <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
-      <MenuPosts withImage={true} />
+      <MenuPosts withImage={true} /> */}
     </div>
   );
 };
--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  return (
+    <div className="flex justify-between mt-2">
+      <button className="w-20 border p-2 bg-[#dc143c] text-white cursor-pointer hover:bg-orange-400 hover:text-black">
+        Previous
+      </button>
+      <button className="w-20 border p-2 bg-[#dc143c] text-white cursor-pointer hover:bg-orange-400 hover:text-black">
+        Next
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
