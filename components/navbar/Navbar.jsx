90e0c1772cbb0a6811f6f081c8632cae445945bb
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2024-03-24 17:10:03 +0530

    test

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -19,9 +19,9 @@ const Navbar = () => {
         <Image src="/facebook.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <button href="/" className={styles.logo}>
+      <Link href="/" className={styles.logo}>
         Sajid-Dev
-      </button>
+      </Link>
       <div className={styles.links}>
         <Link href="/" className={styles.link}>
           Homepage
